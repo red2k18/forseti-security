53662f15b05dcba16dc8ecbfcfa4aace68b67897
Olivier Cervello <ocervello@google.com>
2018-11-26 16:44:16 -0600

    Remove start_span / end_span from run_crawler, replace with decorator instead

--- google/cloud/forseti/services/inventory/crawler.py
+++ google/cloud/forseti/services/inventory/crawler.py
@@ -316,7 +316,6 @@ def run_crawler(storage,
     Returns:
         QueueProgresser: The progresser implemented in inventory
     """
-    # tracing.start_span(tracer, 'inventory', 'run_crawler')
     client_config = config.get_api_quota_configs()
     client_config['domain_super_admin_email'] = config.get_gsuite_admin_email()
 
@@ -338,5 +337,4 @@ def run_crawler(storage,
     progresser = crawler_impl.run(resource)
     # flush the buffer at the end to make sure nothing is cached.
     storage.commit()
-    # tracing.end_span(tracer, **progresser.__dict__)
     return progresser
